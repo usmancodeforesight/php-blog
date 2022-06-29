f497e315e7b64191e06711e8050281d48ad433c1
usmancodeforesight <102722762+usmancodeforesight@users.noreply.github.com>
2022-06-29 16:45:19 +0500

    Finalized according to referance post/tutorial. There are some errors and functionalities left to fix.

--- filtered_posts.php
+++ filtered_posts.php
@@ -24,7 +24,7 @@ if (isset($_GET['topic'])) {
       <hr>
       <?php foreach ($posts as $post) : ?>
         <div class="post" style="margin-left: 0px;">
-          <img src="<?php echo BASE_URL . '/static/images/' . $post['image']; ?>" class="post_image" alt="">
+          <img src="<?php echo BASE_URL . 'static/images/' . $post['image']; ?>" class="post_image" alt="">
           <a href="single_post.php?post-slug=<?php echo $post['slug']; ?>">
             <div class="post_info">
               <h3><?php echo $post['title'] ?></h3>
